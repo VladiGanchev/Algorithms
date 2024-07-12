b429b11521ecb274bf295c53fa9ad1e2f1a17165
VladiGanchev <160647367+VladiGanchev@users.noreply.github.com>
2024-07-12 20:19:14 +0300

    Recursion

--- RecursionAndBacktracking/RecursionAndBacktracking/Program.cs
+++ RecursionAndBacktracking/RecursionAndBacktracking/Program.cs
@@ -2,6 +2,11 @@
 {
     public class Program
     {
+
+        private static HashSet<int> attackedRows = new HashSet<int>();
+        private static HashSet<int> attackedCols = new HashSet<int>();
+        private static HashSet<int> attackedLeftDiagonals = new HashSet<int>();
+        private static HashSet<int> attackedRoghtDiagonals = new HashSet<int>();
         public static void Main(string[] args)
         {
             //Task 1
@@ -46,11 +51,73 @@
 
             //Task 6
 
+            var board = new bool[8, 8];
+
+            PutQueens(board, 0);
 
             //Task 7
             //int n = int.Parse(Console.ReadLine());
             //Console.WriteLine(Fibonacci(n));
 
+        }
+
+        private static void PutQueens(bool[,] board, int row)
+        {
+            if (row >= board.GetLength(0))
+            {
+                PrintBoard(board);
+                return;
+            }
+
+            for (int col = 0; col < board.GetLength(1); col++)
+            {
+                if (CanPlaceQueen(row, col))
+                {
+                    attackedRows.Add(row);
+                    attackedCols.Add(col);
+                    attackedLeftDiagonals.Add(row - col);
+                    attackedRoghtDiagonals.Add(row + col);
+                    board[row, col] = true;
+
+                    PutQueens(board, row + 1);
+
+                    attackedRows.Remove(row);
+                    attackedCols.Remove(col);
+                    attackedLeftDiagonals.Remove(row - col);
+                    attackedRoghtDiagonals.Remove(row + col);
+                    board[row, col] = false;
+                }
+            }
+        }
+
+        private static void PrintBoard(bool[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j])
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+
+        private static bool CanPlaceQueen(int row, int col)
+        {
+            return !attackedRows.Contains(row) &&
+                !attackedCols.Contains(col) &&
+                !attackedLeftDiagonals.Contains(row - col) &&
+                !attackedRoghtDiagonals.Contains(row + col);
         }
 
         private static int Fibonacci(int n)
